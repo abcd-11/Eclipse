1df7f3180730666f7cb7d51b7722069a394c4df8
Mukesh S. Prajapati <mukeshsprajapati210@gmail.com>
2015-08-01 15:27:50 +0530

    second commit

---
+++ workspace/Hackerrank/src/hackerrank/Hello_World.java
@@ -0,0 +1,8 @@
+package hackerrank;
+
+public class Hello_World {
+           public static void main(String args[])
+           {
+        	   System.out.println("Hello World!!!");
+           }
+}
